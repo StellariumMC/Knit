88ff00962db196ad65e873c258f0d46370b8a753
aurielyn <aurielyn.dev@gmail.com>
2025-10-14 21:05:48 +0530

    Add KnitClipboard

---
+++ src/main/kotlin/xyz/meowing/knit/api/KnitClipboard.kt
@@ -0,0 +1,25 @@
+package xyz.meowing.knit.api
+
+import net.minecraft.client.gui.GuiScreen
+
+//#if MC <= 1.12.2
+//$$ import xyz.meowing.knit.Knit
+//#endif
+
+object KnitClipboard {
+    var string: String
+        get() {
+            //#if MC >= 1.16.5
+            //$$ return Knit.client.keyboard.clipboard
+            //#else
+            return GuiScreen.getClipboardString()
+            //#endif
+        }
+        set(value) {
+            //#if MC >= 1.16.5
+            //$$ Knit.client.keyboard.clipboard = value
+            //#else
+            GuiScreen.setClipboardString(value)
+            //#endif
+        }
+}
