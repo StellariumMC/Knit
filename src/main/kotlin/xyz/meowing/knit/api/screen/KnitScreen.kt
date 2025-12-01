1d9bf95a07f54ac35480084a39e0d348731077af
aurielyn <aurielyn.dev@gmail.com>
2025-12-01 20:51:30 +0530

    Feat: allow for custom return values of onMouseClick, onMouseRelease, onKeyType

--- src/main/kotlin/xyz/meowing/knit/api/screen/KnitScreen.kt
+++ src/main/kotlin/xyz/meowing/knit/api/screen/KnitScreen.kt
@@ -44,15 +44,15 @@ abstract class KnitScreen(screenName: String = "Knit-Screen") : Screen(KnitText.
     open fun onRender(context: DrawContext?, mouseX: Int, mouseY: Int, deltaTicks: Float) {}
     //#endif
 
-    open fun onMouseClick(mouseX: Int, mouseY: Int, button: Int) {}
+    open fun onMouseClick(mouseX: Int, mouseY: Int, button: Int): Boolean = false
 
-    open fun onMouseRelease(mouseX: Int, mouseY: Int, button: Int) {}
+    open fun onMouseRelease(mouseX: Int, mouseY: Int, button: Int): Boolean = false
 
     open fun onMouseScroll(horizontal: Double, vertical: Double) {}
 
     open fun onMouseMove(mouseX: Int, mouseY: Int) {}
 
-    open fun onKeyType(typedChar: Char, keyCode: Int, scanCode: Int) {}
+    open fun onKeyType(typedChar: Char, keyCode: Int, scanCode: Int): Boolean = false
 
     final override fun init() {
         onInitGui()
@@ -76,68 +76,68 @@ abstract class KnitScreen(screenName: String = "Knit-Screen") : Screen(KnitText.
     }
 
     //#if MC >= 1.21.9
-    //#if FORGE-LIKE
-    //$$ override fun mouseClicked(click: MouseButtonEvent, doubled: Boolean): Boolean {
-    //#else
-    //$$ override fun mouseClicked(click: Click?, doubled: Boolean): Boolean {
-    //#endif
+        //#if FORGE-LIKE
+        //$$ override fun mouseClicked(click: MouseButtonEvent, doubled: Boolean): Boolean {
+        //#else
+        //$$ override fun mouseClicked(click: Click?, doubled: Boolean): Boolean {
+        //#endif
     //$$     if (click == null) return super.mouseClicked(click, doubled)
-    //$$     onMouseClick(click.x.toInt(), click.y.toInt(), click.button())
-    //$$     return super.mouseClicked(click, doubled)
+    //$$     val handled = onMouseClick(click.x.toInt(), click.y.toInt(), click.button())
+    //$$     return if (handled) true else super.mouseClicked(click, doubled)
     //$$ }
     //#else
     override fun mouseClicked(mouseX: Double, mouseY: Double, button: Int): Boolean {
-        onMouseClick(mouseX.toInt(), mouseY.toInt(), button)
-        return super.mouseClicked(mouseX, mouseY, button)
+        val handled = onMouseClick(mouseX.toInt(), mouseY.toInt(), button)
+        return if (handled) true else super.mouseClicked(mouseX, mouseY, button)
     }
     //#endif
 
     //#if MC >= 1.21.9
-    //#if FORGE-LIKE
-    //$$ override fun mouseReleased(click: MouseButtonEvent): Boolean {
-    //#else
-    //$$ override fun mouseReleased(click: Click?): Boolean {
-    //#endif
+        //#if FORGE-LIKE
+        //$$ override fun mouseReleased(click: MouseButtonEvent): Boolean {
+        //#else
+        //$$ override fun mouseReleased(click: Click?): Boolean {
+        //#endif
     //$$     if (click == null) return super.mouseReleased(click)
-    //$$     onMouseRelease(click.x.toInt(), click.y.toInt(), click.button())
-    //$$     return super.mouseReleased(click)
+    //$$     val handled = onMouseRelease(click.x.toInt(), click.y.toInt(), click.button())
+    //$$     return if (handled) true else super.mouseReleased(click)
     //$$ }
     //#else
     override fun mouseReleased(mouseX: Double, mouseY: Double, button: Int): Boolean {
-        onMouseRelease(mouseX.toInt(), mouseY.toInt(), button)
-        return super.mouseReleased(mouseX, mouseY, button)
+        val handled = onMouseRelease(mouseX.toInt(), mouseY.toInt(), button)
+        return if (handled) true else super.mouseReleased(mouseX, mouseY, button)
     }
     //#endif
 
     //#if MC >= 1.21.9
-    //#if FORGE-LIKE
-    //$$ override fun keyPressed(input: KeyEvent): Boolean {
-    //#else
-    //$$ override fun keyPressed(input: KeyInput?): Boolean {
-    //#endif
+        //#if FORGE-LIKE
+        //$$ override fun keyPressed(input: KeyEvent): Boolean {
+        //#else
+        //$$ override fun keyPressed(input: KeyInput?): Boolean {
+        //#endif
     //$$     if (input == null) return super.keyPressed(input)
-    //$$     onKeyType('\u0000', input.key, input.scancode)
-    //$$     return super.keyPressed(input)
+    //$$     val handled = onKeyType('\u0000', input.key, input.scancode)
+    //$$     return if (handled) true else super.keyPressed(input)
     //$$ }
     //$$
-    //#if FORGE-LIKE
-    //$$ override fun charTyped(input: CharacterEvent): Boolean {
-    //#else
-    //$$ override fun charTyped(input: CharInput?): Boolean {
-    //#endif
+        //#if FORGE-LIKE
+        //$$ override fun charTyped(input: CharacterEvent): Boolean {
+        //#else
+        //$$ override fun charTyped(input: CharInput?): Boolean {
+        //#endif
     //$$     if (input == null) return super.charTyped(input)
-    //$$     onKeyType(input.codepoint.toChar(), 0, 0)
-    //$$     return super.charTyped(input)
+    //$$     val handled = onKeyType(input.codepoint.toChar(), 0, 0)
+    //$$     return if (handled) true else super.charTyped(input)
     //$$ }
     //#else
     override fun keyPressed(keyCode: Int, scanCode: Int, modifiers: Int): Boolean {
-        onKeyType('\u0000', keyCode, scanCode)
-        return super.keyPressed(keyCode, scanCode, modifiers)
+        val handled = onKeyType('\u0000', keyCode, scanCode)
+        return if (handled) true else super.keyPressed(keyCode, scanCode, modifiers)
     }
 
     override fun charTyped(chr: Char, modifiers: Int): Boolean {
-        onKeyType(chr, 0, 0)
-        return super.charTyped(chr, modifiers)
+        val handled = onKeyType(chr, 0, 0)
+        return if (handled) true else super.charTyped(chr, modifiers)
     }
     //#endif
 
